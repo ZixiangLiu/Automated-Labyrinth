776bb5c7d2f4061172567bccf439a6228a24c094
ZixiangLiu <zixiangliu2@gmail.com>
2018-02-28 20:51:20 -0500

    first commit

---
+++ Maze/CameraController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraController.swift
+//  Maze
+//
+//  Created by Zixiang Liu on 2/28/18.
+//  Copyright © 2018 Zixiang Liu. All rights reserved.
+//
+
+import Foundation
---
+++ Maze/PreviewController.swift
@@ -0,0 +1,9 @@
+//
+//  PreviewController.swift
+//  Maze
+//
+//  Created by Zixiang Liu on 2/28/18.
+//  Copyright © 2018 Zixiang Liu. All rights reserved.
+//
+
+import Foundation
